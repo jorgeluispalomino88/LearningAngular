25b6308c60707e3190a7350c6367a6b25f432bbe
Jorge Luis Palomino <jorgeluispalomino88@gmail.com>
2021-04-22 22:14:34 -0400

    Implemented data and events transference between components
    Implemented delete item

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { Item } from 'src/app/model/item';
 
 @Component({
@@ -8,11 +9,26 @@ import { Item } from 'src/app/model/item';
 })
 export class ItemComponent implements OnInit {
 
+  //Parametro de entrada. Para pasarle datos al componente desde su componente padre
   @Input() item: Item = new Item();
+
+  //Parametro de salida. Para enviar eventos hacia el componente padre
+  @Output() deleteItem: EventEmitter<Item> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+ //Al hacer click al boton de eliminar se ejecuta el metodo onDelete
+ //que dispara el evento deleteItem con el Item seleccionado
+  onDelete(item: Item){
+    console.log("Executed  method onDelete");
+    this.deleteItem.emit(item);
+  }
+
+  onToggle(item: Item){
+    item.completed = !item.completed;
   }
 
 }
--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -23,13 +23,19 @@ export class ItemsComponent implements OnInit {
           completed: true
       },
       {
-        id: 0,
+        id: 1,
         title: 'platano',
         price: 5.0,
         quantity: 10,
         completed: false
     }
     ];
+  }
+
+  //Metodo que captura el evento deleteItem del componente Item y elimina el componente de la lista de Items
+  deleteItem(item: Item){
+    console.log("Deleting item " + item.id);
+    this.items = this.items.filter(x => x.id != item.id);
   }
 
 }
